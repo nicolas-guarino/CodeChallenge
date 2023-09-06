6862481b519c9d50a417f2b4ab5c8b6692448092
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-09-05 21:49:52 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,44 +1,38 @@
 const url = "https://jsonplaceholder.typicode.com/users";
-
 const btn = document.getElementById('btn');
 
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
 
-btn.addEventListener('click', function() {
     const name = document.getElementById('name').value;
     const lastName = document.getElementById('lastName').value;
     const date = document.getElementById('date').value;
 
-    datos.name = name;
-    datos.lastName = lastName;
-    datos.date = date;
+    const datos = {
+        name: name,
+        lastName: lastName,
+        date: date
+    };
+
+    const promise = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    };
+
+    fetch(url, promise)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Fallo el envío');
+            }
+            return response.json();
+        })
+        .then(datos => {
+            console.log('Respuesta del servidor:', datos);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 });
-
-const datos = {
-    name: "",
-    lastName: "",
-    date: ""
-};
-
-const promise = {
-    method: "POST",
-    Headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(datos)
-}
-
-fetch(url, promise)
-    .then(Response => {
-        if (!Response.ok) {
-            throw new Error('fallo el envio');
-        }
-        return Response.json();
-    })
-
-    .then(datos => {
-        console.log('Respuesta del servidor:', datos)
-    })
-
-    .catch(error => {
-        console.error('Error', error)
-    })
